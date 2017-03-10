f98d5be291d4383719de6308ecde7dfd40407e18
Steve Kane <kanesteven@gmail.com>
2017-03-09 20:07:32 -0600

    wrote extensive notes about thoughts on reducing the cost of the evaluator

--- src/main.js
+++ src/main.js
@@ -16,6 +16,79 @@ var DARK_GRAY = [ .1, .1, .1, 1 ]
 document.body.appendChild(GL_LAYER)
 document.body.appendChild(UI_LAYER)
 
+/*
+Evaluation: Compute Composite SDF for every position in the framebuffer
+N, M = width and height of framebuffer
+L    = Length of edits
+C    = N * M * C
+
+There is absolutely no dependence on other cells to compute the value of a given cell
+
+Therefore, if we had N x M threads, we could compute the result for a given cell in parallel
+
+There are two causes of inefficiency here:  
+
+1. We're calculating the contribution of SDFs that are very far away from the cell.  
+   This means they will have a non-visual effect on that cell and probably should be culled.
+2. We need to deal with the data supplying the SDF field changing every frame.  This means
+   we cannot simply compile a "hard-coded" shader to represent the SDF because the cost of
+   re-compilation is too high ( especially true in webgl ).  Furthermore, this cost would 
+   not scale well if we have many objects in the scene.
+
+We can store the editlist in a texture in order to by-pass the size-limitations of uniforms.
+We would then need to loop over that texture in the fragment shader sampling all of its contents
+( or breaking early if a certain trigger value is detected representing un-used edits ).  
+We would then be able to do N * M * L computations for our scene.  This number is likely to be way
+too high to be fast enough to execute in our main-loop.  
+
+It's worth pointing out here as well that we're trying to support large numbers of edits ( 1e4 )
+
+If we extend this reasoning into 3d ( the real goal overall ) we would have another factor of D ( depth )
+which would mean our cost is enormous to compute per-frame.  Thus, we almost certainly must be ruthless
+about performing only the meaningful SDF computations for a given cell.  An example calculation below:
+
+N = 1024 ( 2^10 )
+M = 1024 ( 2^10 )
+L = 4096 ( 2^12 )
+
+T = N * M * L == 2^32 ~ 4billion
+
+This means that even if we can save 99% of this cost through some sort of partitioning scheme, we are still
+trying to do 42million computations
+
+Let's also call out the min-bound here where we have an editlist of 1 edit.  
+
+Tmin = N * M * 1 == 2^20 ~ 1million
+
+It's worth pointing out that the above calculates the contribution of a single edit across all the cells.
+
+If we assume that the average size of an edit is ~10% of the available cells ( not sure if this is correct 
+but as a heuristic it might not be crazy ) and assume we can somehow cull the edits from cells that are 
+far away:
+
+This allows us to drop the total computations by a factor of 10 meaning we'd be sitting at an average cost
+of ~100,000 computations.  Or more generally, we can reduce the number of the computations by this same heuristic
+for the general case which saves us ~2^3 computations.
+
+Thus, for the L above (4096), we would have 2^29 ~500million calculations
+
+It seems therefore that we must do even more culling.  Perhaps there is a final opportunity here for any cell 
+that already is fully overlapped.  We can simply recognize that no additional cells will provide a meaningful 
+contribution to the distance field value at that point.  This means that, in fact, we only need to store more than
+one edit in cells for which no overlapping object has already been identified.  Maybe this can be done w/ a mask?
+We could use a shader to read every edit once and splat out an mask for each shape (along with the index of the shape )
+that was responsible for masking that region.  Then, we could run a second shader which only considers the contributions
+of nearby influencing shapes if the mask value for that region is not saturated.
+
+If this works, you could wildly reduce the cost down to the base-case which is ~ N x M or ~1million.
+
+This ALSO begs the question if the correct storage of the computed SDF is even a voxel texture at all?  Maybe there
+is a more sparse data structure that would deal more efficiently with voids and areas of higher complexity?  This 
+possibility is as-yet un-explored but it might be very worthwhile to explore.  It seems that CPU implementations
+might be easier to explore than GPU while formulating a strategy for reducing the cost.
+*/
+
+
 Object.assign(document.documentElement.style, {
   height: '100%'
 })
@@ -39,7 +112,7 @@ Object.assign(GL_LAYER.style, {
 })
 
 var regl = Regl({
-  extensions: [ 'OES_texture_float', 'WEBGL_draw_buffers' ],
+  extensions: [ 'OES_texture_float' ],
   container: GL_LAYER
 })
 var bt = BIG_TRIANGLE(4)
--- test-monoid.js
+++ test-monoid.js
@@ -1,22 +1,26 @@
 const { red, white, green } = require('chalk')
-const { sqrt, pow, min, max } = Math
-
-const length = v => sqrt(pow(v[0], 2) + pow(v[1], 2))
-const empty = _ => 1
-const mappend = (p, e1, e2) => _ => min(e1(p), e2(p))
+const { floor, sqrt, pow, min, max, abs } = Math
 
 const BOUND = 1
 const CELL_SIZE = .1
+
+const lerp = ( min, max, i ) => min + i * ( max - min )
+const zipWith = f => ( v1, v2 ) => v1.map((c, i) => f(c, v2[i]))
+const vlength = v => sqrt(pow(v[0], 2) + pow(v[1], 2))
+const vsub = zipWith(( x, y ) => x - y)
+const vadd = zipWith(( x, y ) => x + y)
+const vabs = v => v.map(abs)
+const circle  = ( c, r ) => p => vlength(vsub(p, c)) - r
+const empty = _ => 1
+const mappend = ( p, e1, e2 ) => _ => min(e1(p), e2(p))
+const compute = ( es, p ) => es.reduce(mappend.bind(null, p), empty)
+
 const edits = [ 
-  p => length(p) - .5,
-  p => length([ p[0] - 1, p[1] - 1 ]) - .5,
-  p => length([ p[0] + 1, p[1] + 1 ]) - .5
+  circle([ 0, 0 ], .2),
+  circle([ .5, .5 ], .2)
 ]
 
-function compute ( edits, p ) {
-  return edits.reduce(mappend.bind(null, p), empty)
-}
-
+// TODO: TYPICALLY THE RESOLUTION IS FIXED.  maybe change this?
 function voxelize ( left, right, bottom, top, spacing, edits ) {
   var i, j, d, out = []
 
@@ -28,6 +32,25 @@ function voxelize ( left, right, bottom, top, spacing, edits ) {
   }
   return out
 }
+
+// TODO: not working.  consider fixing or just delete?  not really important..
+function vox2 ( fb, edits ) {
+  var { w, h } = fb
+  var left = bottom = -1
+  var right = top = 1
+  var rangex = right - left
+  var rangey = top - bottom
+
+  for ( var i = 0, cx, cy, x, y; i < w * h; i++ ) {
+    cx = floor(i / w)
+    cy = i % w
+    x = lerp(left, right, cx / rangex)
+    y = lerp(bottom, top, cy / rangey)
+    console.log({ cx, cy, x, y })
+  }
+}
+
+vox2({ w: 4, h: 4 })
 
 function print ( voxels ) {
   for ( var i = 0; i < voxels.length; i++ ) {
@@ -43,4 +66,4 @@ function print ( voxels ) {
 
 const voxels = voxelize(-BOUND, BOUND, -BOUND, BOUND, CELL_SIZE, edits)
 
-print(voxels)
+// print(voxels)
